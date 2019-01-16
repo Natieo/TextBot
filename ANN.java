97ae77d450e900d85393d6c46c8f48a603e875e0
Natieo <thenateanateor@gmail.com>
2019-01-16 11:15:20 -0800

    Create ANN.java

---
+++ ANN.java
@@ -0,0 +1,8 @@
+public class ANN
+{
+  private double[/*Node*/] x;
+  private double[/*Layer*/][/*Node*/] hs;
+  private double[/*Layer*/][/*Node*/] hz;
+  private double[/*Node*/] oz;
+  private double[/*Node*/] yhat;
+}
